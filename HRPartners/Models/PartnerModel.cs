2abfc171078f4e83dc2970a608baed19466d5ba3
Dipo Olakan <Dipo.Olakan@fidelitybank.ng>
2018-11-27 12:18:38 +0100

    Add project files.

---
+++ HRPartners/Models/PartnerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRPartners.Models
+{
+    public class PartnerModel
+    {
+        public int PartnersId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal EstimatedValue { get; set; }
+    }
+}
---
+++ HRPartners/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRPartners.Startup))]
+namespace HRPartners
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
