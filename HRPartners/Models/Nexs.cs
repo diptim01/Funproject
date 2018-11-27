4e910c49af37ebf75ca860a2fef5bf5c53b09eed
Dipo Olakan <Dipo.Olakan@fidelitybank.ng>
2018-11-27 12:18:38 +0100

    Add project files.

---
+++ HRPartners/Models/Nexs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRPartners.Models
+{
+    public class Nexs
+    {
+        public int NexsId { get; set; }
+        public int PartnersId { get; set; }
+        public int FicBudget { get; set; }
+        public int SeismicBudget { get; set; }
+        public int ExpenditureBudget { get; set; }
+
+
+    }
+}
